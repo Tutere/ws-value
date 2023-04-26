cc96b81b24b257b623c295ee94a4d73ca7126f5e
debhjy <debhjy@gmail.com>
2023-04-26 17:10:23 +1200

    format changes

--- src/pages/monthlyReport.tsx
+++ src/pages/monthlyReport.tsx
@@ -29,10 +29,10 @@ export default function CalendarDateRangePicker({
           <Button
             id="date"
             variant={"outline"}
-            className={cn(
-              "w-[300px] justify-start text-left font-normal",
-              !date && "text-muted-foreground"
-            )}
+            // className={cn(
+            //   "w-[300px] justify-start text-left font-normal",
+            //   !date && "text-muted-foreground"
+            // )}
           >
             <CalendarIcon className="mr-2 h-4 w-4" />
             {date?.from ? (
