c77bbc065e49fa0329ec552ca93f827f44b9efff
tuteredurie <innovation@worksafe.govt.nz>
2023-05-25 17:37:58 +1200

    new buttons added to activity details page

--- src/pages/activity/[activityID].tsx
+++ src/pages/activity/[activityID].tsx
@@ -63,7 +63,7 @@ const toggleReadMore = () => {
 }
 
   if(loading) {
-    <LoadingPage></LoadingPage>
+    return <LoadingPage></LoadingPage>
   }
   if (activity === null || activity === undefined ) {
     return <p>Error finding activity</p>
@@ -73,9 +73,12 @@ const toggleReadMore = () => {
     {isMemberFound ? (
     <div className="p-8 bg-white rounded-lg shadow-md">
       
-      <Link href={"/" + project?.id}>
-        <Button className="mb-5" variant={"subtle"}>
-         {"< Back to project"}
+      <Link href={"/" + project?.id} onClick={() => setLoading(true)}>
+        <Button className="mb-5" variant={"withIcon"}>
+        <svg fill="currentColor" className="w-4 h-4 mr-2 fill-current"  viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+          <path clip-rule="evenodd" fill-rule="evenodd" d="M17 10a.75.75 0 01-.75.75H5.612l4.158 3.96a.75.75 0 11-1.04 1.08l-5.5-5.25a.75.75 0 010-1.08l5.5-5.25a.75.75 0 111.04 1.08L5.612 9.25H16.25A.75.75 0 0117 10z"></path>
+        </svg>
+        {"Back to project"}
         </Button>
       </Link>
        
@@ -165,15 +168,23 @@ const toggleReadMore = () => {
       size={"sm"}
       onClick={toggleReadMore}> {!isReadMoreShown ? "See More.." : "See Less..."}
       </Button>
-      
+
       <div className="mt-10 flex gap-7"> 
-      <Link href={"/editActivity/" + id} onClick={() => setLoading(true)}>
-        <Button variant={"default"}>
-            Edit Activity
-        </Button>
-      </Link>
-      <DeletionDialog object="Activity" id={id} handleDelete={ActivityhandleDelete}></DeletionDialog>
-      </div>
+      <div className="inline-flex rounded-md shadow-sm mt-2" role="group">
+          <Link href={"/editActivity/" + id} onClick={() => setLoading(true)} type="button" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border-l border-t border-b border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+          <svg fill="currentColor" className="w-4 h-4 mr-2 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+              <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32l8.4-8.4z"></path>
+              <path d="M5.25 5.25a3 3 0 00-3 3v10.5a3 3 0 003 3h10.5a3 3 0 003-3V13.5a.75.75 0 00-1.5 0v5.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5V8.25a1.5 1.5 0 011.5-1.5h5.25a.75.75 0 000-1.5H5.25z"></path>
+            </svg>   
+          Edit Activity        
+          </Link>
+
+
+          <DeletionDialog object="Project" id={id} handleDelete={ActivityhandleDelete}></DeletionDialog> 
+
+        </div>
+        </div>
+
     </div>
     
     ): (
