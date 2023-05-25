8394dad91e12b5b4e2b62337ff1b0dd2c3ef8b18
tuteredurie <innovation@worksafe.govt.nz>
2023-05-26 08:39:23 +1200

    buttons changed on all pages

--- src/components/ui/emailConfirmation.tsx
+++ src/components/ui/emailConfirmation.tsx
@@ -32,7 +32,11 @@ export function EmailConfirmation(props: {emailSending: boolean | undefined, sen
   return (
     <Dialog open={isOpen}>
       <DialogTrigger>
-        <Button className="bg-green-600 mt-4" onClick={() => setIsOpen(true)} disabled={props.emailSending}>
+        <Button variant={"default"} className="bg-green-600 mt-4" onClick={() => setIsOpen(true)} disabled={props.emailSending}>
+        <svg fill="currentColor" className="w-4 h-4 mr-2 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+          <path d="M3 4a2 2 0 00-2 2v1.161l8.441 4.221a1.25 1.25 0 001.118 0L19 7.162V6a2 2 0 00-2-2H3z"></path>
+          <path d="M19 8.839l-7.77 3.885a2.75 2.75 0 01-2.46 0L1 8.839V14a2 2 0 002 2h14a2 2 0 002-2V8.839z"></path>
+        </svg>
         {props.emailSending ? "Sending Email..." : " Send Email"}
         </Button>
       </DialogTrigger>
