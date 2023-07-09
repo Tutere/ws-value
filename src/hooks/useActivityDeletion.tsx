7c26efd5689c8a97f9c399b611d0b5fb33656ff5
tuteredurie <innovation@worksafe.govt.nz>
2023-07-10 11:48:01 +1200

    activity tracking working for actyivity deletion. Logic moved to backend

--- src/hooks/useActivityDeletion.tsx
+++ src/hooks/useActivityDeletion.tsx
@@ -7,100 +7,34 @@ import { api } from "~/utils/api";
 
 
 export function useActivityDeletion(id:string) {
-    const router = useRouter();
+  const utils = api.useContext().activities;
 
-    const query = api.projects.findByActivityId.useQuery(
-        { id: id },
-        {
-          suspense: true,
-        }
-      );
+  const router = useRouter();
+
+  const query = api.projects.findByActivityId.useQuery({ id: id },{
+      suspense: true,
+    }
+  );
     
-      const project = query.data;
+  const project = query.data;
 
-   //activity handling
-  const utilsActivities = api.useContext().activities;
-  const mutationActivities = api.activities.delete.useMutation({
+  const { mutate: ActivityhandleDelete } = api.activities.softDeleteByActivityId.useMutation({
     onSuccess: async () => {
-      await utilsActivities.read.invalidate();
+      await utils.read.invalidate();
+      router.push("/" + project?.id);
     },
   });
 
-  const mutationSpecificActivy = api.activities.softDeleteByActivityId.useMutation({
-    onSuccess: async () => {
-      await utilsActivities.read.invalidate();
-    },
-  });
-
-  const methodsActivities = useZodForm({
-    schema: ReadActivitySchema,
-    defaultValues: {
-      projectId: id,
-    },
-  });
-
-  const methodSpecificActivity = useZodForm({
-    schema: ReadSpecificActivitySchema,
-    defaultValues: {
-      id: id,
-    },
-  });
-
-  //data lineage handling for activity
-  const queryProjectById = api.projects.findByProjectId.useQuery(
-    { id: id },
-    {
-      suspense: true,
-    }
-  );
-
-  const project2 = queryProjectById.data;
-
-  const mutationActivityracker = api.activityTracker.edit.useMutation();
-
-  const queryActivityById = api.activities.readSpecific.useQuery(
-    { id: id },
-    {
-      suspense: true,
-    }
-  );
-
-  const activity = queryActivityById.data;
-
-  const methodActivityTracker = useZodForm({
-    schema: ActivityChangeSchema,
-    defaultValues: {
-      changeType: "Delete",
-      id: activity?.id,
-      projectId: activity?.projectId.toString(),
-      name: activity?.name?.toString(),
-      description: activity?.description?.toString(),
-      engagementPattern: activity?.engagementPattern?.toString(),
-      valueCreated: activity?.valueCreated?.toString(),
-      startDate: activity?.startDate?.toISOString(),
-      endDate: activity?.endDate?.toISOString() || "",
-      outcomeScore: activity?.outcomeScore!,
-      effortScore: activity?.effortScore!,
-      status: activity?.status!,
-      hours: activity?.hours!,
-      members: activity?.members?.map((member) => member.projectMemberId),
-      stakeholders: project2?.stakeholders!,
-      reportComments:activity?.reportComments?? "",
-    },
-  });
-
+  return { ActivityhandleDelete };
   
-    const ActivityhandleDelete = async () => {
-        await Promise.all([
-            await mutationActivityracker.mutateAsync(
-              methodActivityTracker.getValues()
-            ),
-            await mutationSpecificActivy.mutateAsync(
-              methodSpecificActivity.getValues()
-            ),
-          ]);
-          router.push("/" + project?.id);
-    };
+    // const ActivityhandleDelete = async () => {
+    //     await Promise.all([
+    //         await mutationSpecificActivy.mutateAsync(
+    //           methodSpecificActivity.getValues()
+    //         ),
+    //       ]);
+    //       router.push("/" + project?.id);
+    // };
   
-    return { ActivityhandleDelete };
+    // return { ActivityhandleDelete };
   }
